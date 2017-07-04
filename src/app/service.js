6e18f7c0060d07fff797ffd170c1238b7972d2e1
Francesco Corvino <francesco.corvino@csebo.it>
2017-07-04 12:11:31 +0200

    Strutturazione degli eventi

--- src/app/service.js
+++ src/app/service.js
@@ -6,7 +6,28 @@ angular.module('locomotivejazz.service', [])
 .service('EventiServizio',function(){
     var servizio = {};
     var servizi = [
-        { luogo: '1.1', id: 1 , data: new Date() ,  image:'img/event1.png' , isEventoInEvidenza: true  },
+        {
+            image:'img/event1.png',
+            id: 1,
+            titolo: "La musica nasce dalle periferie",
+            data: "22 Luglio",
+            luogo: "Monteroni di Lecce",
+            luogoMaps: {
+                lat: 343,
+                log: 3434,
+                titolo: "Palazzo Baronale"
+            },
+            programma: [
+                {
+                    ore: "11:00",
+                    descrizione: "conferenza <i>\"La Musica nasce dalle Periferie\"</i><br/> in collaborazione con Università del Salento"
+                },
+                {
+                    ore: "20:30",
+                    descrizione: "conferenza \"La Musica nasce dalle Periferie\"<br/> in collaborazione con Università del Salento"
+                },
+            ]
+        },
         { luogo: '2.2', id: 2 , data: new Date() ,   image:'img/evento2.png' , isEventoInEvidenza: false },
         { luogo: '3.3', id: 3 , data: new Date() ,   image:'img/evento3.png', isEventoInEvidenza: false  },
         { luogo: '4.4', id: 4 , data: new Date() ,   image:'img/event1.png' , isEventoInEvidenza: true  },
