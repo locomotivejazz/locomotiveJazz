41ac9cc81198826e7cf348e5ab6709d3c44b7654
matteonestola <matty.nestola@gmail.com>
2017-07-04 14:20:26 +0200

    aggiornamento specifiche evento

--- src/app/service.js
+++ src/app/service.js
@@ -72,11 +72,192 @@ angular.module('locomotivejazz.service', [])
                 },
             ]
         },
-        { luogo: '2.2', id: 4 , data: new Date() ,   image:'img/evento2.png' , isEventoInEvidenza: false },
-        { luogo: '3.3', id: 5 , data: new Date() ,   image:'img/evento3.png', isEventoInEvidenza: false  },
-        { luogo: '4.4', id: 6 , data: new Date() ,   image:'img/event1.png' , isEventoInEvidenza: true  },
-        { luogo: '5.5', id: 7 },
-        { luogo: '6.6', id: 8 }
+        {
+            image:'img/event1.png',
+            id: 4,
+            titolo: "La musica nasce dalle periferie",
+            data: "14 Luglio",
+            luogo: "Monteroni di Lecce",
+            luogoMaps: {
+                lat: 343,
+                log: 3434,
+                titolo: "Palazzo Baronale"
+            },
+            programma: [
+                {
+                    ore: "11:00",
+                    descrizione: "conferenza <i>\"La Musica nasce dalle Periferie\"</i><br/> in collaborazione con Università del Salento"
+                },
+                {
+                    ore: "20:30",
+                    descrizione: "conferenza \"La Musica nasce dalle Periferie\"<br/> in collaborazione con Università del Salento"
+                },
+            ]
+        },
+        {
+            image:'img/event1.png',
+            id: 5,
+            titolo: "La musica nasce dalle periferie",
+            data: "14 Luglio",
+            luogo: "Monteroni di Lecce",
+            luogoMaps: {
+                lat: 343,
+                log: 3434,
+                titolo: "Palazzo Baronale"
+            },
+            programma: [
+                {
+                    ore: "11:00",
+                    descrizione: "conferenza <i>\"La Musica nasce dalle Periferie\"</i><br/> in collaborazione con Università del Salento"
+                },
+                {
+                    ore: "20:30",
+                    descrizione: "conferenza \"La Musica nasce dalle Periferie\"<br/> in collaborazione con Università del Salento"
+                },
+            ]
+        },
+        {
+            image:'img/event1.png',
+            id: 6,
+            titolo: "La musica nasce dalle periferie",
+            data: "14 Luglio",
+            luogo: "Monteroni di Lecce",
+            luogoMaps: {
+                lat: 343,
+                log: 3434,
+                titolo: "Palazzo Baronale"
+            },
+            programma: [
+                {
+                    ore: "11:00",
+                    descrizione: "conferenza <i>\"La Musica nasce dalle Periferie\"</i><br/> in collaborazione con Università del Salento"
+                },
+                {
+                    ore: "20:30",
+                    descrizione: "conferenza \"La Musica nasce dalle Periferie\"<br/> in collaborazione con Università del Salento"
+                },
+            ]
+        },
+        {
+            image:'img/event1.png',
+            id: 7,
+            titolo: "La musica nasce dalle periferie",
+            data: "14 Luglio",
+            luogo: "Monteroni di Lecce",
+            luogoMaps: {
+                lat: 343,
+                log: 3434,
+                titolo: "Palazzo Baronale"
+            },
+            programma: [
+                {
+                    ore: "11:00",
+                    descrizione: "conferenza <i>\"La Musica nasce dalle Periferie\"</i><br/> in collaborazione con Università del Salento"
+                },
+                {
+                    ore: "20:30",
+                    descrizione: "conferenza \"La Musica nasce dalle Periferie\"<br/> in collaborazione con Università del Salento"
+                },
+            ]
+        },
+        {
+            image:'img/event1.png',
+            id: 8,
+            titolo: "La musica nasce dalle periferie",
+            data: "14 Luglio",
+            luogo: "Monteroni di Lecce",
+            luogoMaps: {
+                lat: 343,
+                log: 3434,
+                titolo: "Palazzo Baronale"
+            },
+            programma: [
+                {
+                    ore: "11:00",
+                    descrizione: "conferenza <i>\"La Musica nasce dalle Periferie\"</i><br/> in collaborazione con Università del Salento"
+                },
+                {
+                    ore: "20:30",
+                    descrizione: "conferenza \"La Musica nasce dalle Periferie\"<br/> in collaborazione con Università del Salento"
+                },
+            ]
+        },
+        {
+            image:'img/event1.png',
+            id: 9,
+            titolo: "La musica nasce dalle periferie",
+            data: "14 Luglio",
+            luogo: "Monteroni di Lecce",
+            luogoMaps: {
+                lat: 343,
+                log: 3434,
+                titolo: "Palazzo Baronale"
+            },
+            programma: [
+                {
+                    ore: "11:00",
+                    descrizione: "conferenza <i>\"La Musica nasce dalle Periferie\"</i><br/> in collaborazione con Università del Salento"
+                },
+                {
+                    ore: "20:30",
+                    descrizione: "conferenza \"La Musica nasce dalle Periferie\"<br/> in collaborazione con Università del Salento"
+                },
+            ]
+        },
+        {
+            image:'img/event1.png',
+            id: 10,
+            titolo: "La musica nasce dalle periferie",
+            data: "14 Luglio",
+            luogo: "Monteroni di Lecce",
+            luogoMaps: {
+                lat: 343,
+                log: 3434,
+                titolo: "Palazzo Baronale"
+            },
+            programma: [
+                {
+                    ore: "11:00",
+                    descrizione: "conferenza <i>\"La Musica nasce dalle Periferie\"</i><br/> in collaborazione con Università del Salento"
+                },
+                {
+                    ore: "20:30",
+                    descrizione: "conferenza \"La Musica nasce dalle Periferie\"<br/> in collaborazione con Università del Salento"
+                },
+            ]
+        },
+        {
+            image:'img/event1.png',
+            id: 11,
+            titolo: "La musica nasce dalle periferie",
+            data: "14 Luglio",
+            luogo: "Monteroni di Lecce",
+            luogoMaps: {
+                lat: 343,
+                log: 3434,
+                titolo: "Palazzo Baronale"
+            },
+            programma: [
+                {
+                    ore: "11:00",
+                    descrizione: "conferenza <i>\"La Musica nasce dalle Periferie\"</i><br/> in collaborazione con Università del Salento"
+                },
+                {
+                    ore: "20:30",
+                    descrizione: "conferenza \"La Musica nasce dalle Periferie\"<br/> in collaborazione con Università del Salento"
+                },
+            ]
+        },
+        { luogo: '2.2',   id: 4 ,  image:'img/evento2.png'    },
+        { luogo: '3.3',   id: 5 ,  image:'img/evento3.png'    },
+        { luogo: '4.4',   id: 6 ,  image:'img/event1.png'     },
+        { luogo: '5.5',   id: 7 ,  image:'img/event1.png'     },
+        { luogo: '6.6',   id: 8 ,  image:'img/event1.png'     },
+        { luogo: '7.7',   id: 9 ,  image:'img/evento2.png'    },
+        { luogo: '8.8',   id: 10 ,  image:'img/evento2.png'   },
+        { luogo: '9.9',   id: 11,  image:'img/evento2.png'    },
+        { luogo: '10.10', id: 12 ,  image:'img/evento2.png'   },
+        { luogo: '11.11', id: 13,  image:'img/evento2.png'    },
     ];
 
     servizio.getEventi = function(){
