91ed281caf1af1e56f7e8f19efb26a8d53cc747b
matteonestola <matty.nestola@gmail.com>
2017-07-05 16:00:29 +0200

    evento 3 finito

--- src/app/service.js
+++ src/app/service.js
@@ -90,6 +90,66 @@ angular.module('locomotivejazz.service', [])
 
           ]
       },
+      {
+          image:'img/evento3.png',
+          id: 3,
+          titolo: "FROM STATION TO STATION VINTAGE EDITION 2017",
+          data: "23 LUGLIO",
+          luogo: "da BARI a MATERA",
+          luogoMaps: {
+              lat: 343,
+              log: 3434,
+              titolo: ""
+          },
+          programma: [
+              {
+                  ore: "ore 17:00",
+                  descrizione: "<p> inaugurazione e viaggio di una Locomotiva a vapore del 1932 da Bari a Matera  </p>"+
+                               "<p> From Station to Station da Bari a Matera  </p>"+
+                               "<p> “Binario Sonoro Puglia - Basilicata” </p>"+
+                               "<p> partenza dalla stazione di Bari Centrale</p>"+
+                               "<p> <b>MIRKO SIGNORILE</b> piano solo </p>"
+              },
+              {
+                  titolo:"<p> TRILOGIA </p>"+
+                  "<p>TRAMONTO LOCOMOTIVE 2017</p>",
+                  ore: "ore 17:30/Partenza TRENO",
+                  descrizione: "<p> prima sosta Grumo Appula </p>"+
+                                "<p> seconda sosta (degustazione) Altamura  </p>"
+
+              },
+              {
+                  luogo:"",
+                  ore: "ore 21:30/Arrivo a Matera Villa Longo",
+                  descrizione: "<p>POSTI DISPONIBILI n° 121, seconda classe € 20</p>"+
+                               "<p>In vendita su <b> www.rotaiedipuglia.it</b></p>"+
+                               "<p>info: Luigi Mighali 3471225188</p>"
+              },
+              {
+                  ore: "ore 21:30/Arrivo a Matera, Stazione Villa Longo",
+                  descrizione: "<p> passeggiata sonora tra i Sassi  </p>"+
+                               "<p> con <b> ‘LOCOMOTIVE ORCHESTRA’</b>  </p>"+
+                               "<p> e la partecipazione speciale </p>"+
+                               "<p>dell’attrice <b>STEFANIA ROCCA</b></p>"
+              },
+              {
+                  ore: "",
+                  luogo: ">>Casa Cava/INGRESSO LIBERO",
+                  descrizione: "<p> LIVE JAM SESSION  </p>"+
+                               "<p> <i>Un evento Ferrovie Appulo Lucane e con la collaborazione</i> </p>"+
+                               "<p> <i>di Gezziamoci Festival Ass. Onyx jazz e Ass. Biagio Mattatelli</i> </p>"
+              },
+              {
+                  ore: "",
+                  descrizione: "<p class='green-text'> Ritorno a Bari con gli autobus  </p>"+
+                               "<p class='green-text'> delle Ferrovie Appulo Lucane </p>"
+              },
+              {
+                  descrizione: "PREVENDITE SU BOOKINGSHOW.IT"
+              }
+
+          ]
+      },
     ];
 
     servizio.getEventi = function(){
