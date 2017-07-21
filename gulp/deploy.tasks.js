ec2edd1033900e55780485aa790b7ccdd273238e
Francesco Corvino <francesco.corvino@csebo.it>
2017-07-21 10:09:55 +0200

    Aggiornamento versione 1.1

--- gulp/deploy.tasks.js
+++ gulp/deploy.tasks.js
@@ -23,11 +23,15 @@ gulp.task('deploy:prod:android',['build'],function(cb){
     var dateStr = date.getDate()  + (date.getMonth() + 1) + date.getFullYear();
     var fileApk = "./platforms/android/build/outputs/apk/",
         nameApk = "android-release-unsigned.apk",
-        outputFile = "LocomotiveJazz_1.0_" + dateStr + ".apk";
+        outputFile = "LocomotiveJazz_1.1_" + dateStr + ".apk";
 
     var androidBuild = "ionic build android --release";
+    /*
     var apkJarsign = "jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 " +
-        "-keystore keystore/links.keystore -storepass 12345678 \"" + fileApk + nameApk + "\" paskey";
+        "-keystore keystore/links.keystore -storepass Links2016 \"" + fileApk + nameApk + "\" paskey";
+        */
+    var apkJarsign = "jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 " +
+        "-keystore keystore/LinksBGStore.keystore -storepass Links2016 \"" + fileApk + nameApk + "\" LinksBGStore";
     var apkZipalign = "./zipalign -f -v 4 \"" + fileApk + nameApk +  "\" " +
         "\"" + outputFile +"\"";
 
